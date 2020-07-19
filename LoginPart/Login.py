7a41c2bdb5fe7374b4bc9b9798a643999bf76412
mahdi <mahdi.sabour@aut.ac.ir>
2020-07-19 16:49:06 +0430

    sign up part

--- LoginPart/Login.py
+++ LoginPart/Login.py
@@ -1,8 +1,11 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog, QInputDialog
-from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog, QInputDialog, QLabel
 import os
 import sys
 import csv
+import signupPart.signUp as signup
+from PyQt5 import uic, QtCore, QtGui
+from time import sleep
+from signupPart.CustomLineEdit import LineEdit as CustomLineEdit
 
 
 class LoginDatabase():
@@ -35,36 +38,59 @@ class LoginWindow(QDialog, Form):
 
         self.LoginButton.clicked.connect(self.Login)
         self.LoginButton.setEnabled(False)
+        self.signButton.setEnabled(False)
+        self.signButton.clicked.connect(self.signUp)
         self.clearButton.clicked.connect(self.clear)
         self.clearButton.setEnabled(False)
         self.lineEdit_StudentID.textChanged.connect(self.StudentID_update)
         self.lineEdit_Email.textChanged.connect(self.Email_update)
+        self.label_Forget.mousePressEvent = self.forget_Pass
+        self.Label_Msg = QLabel(self)
+        self.Label_Msg.setVisible(False)
+        self.Label_Msg.resize(240, 40)
+        self.Label_Msg.move(self.size().width()/2 - 120, self.size().height()/2 - 90)
+        self.confirm_LineEdit = CustomLineEdit(self)
+        self.confirm_LineEdit.returnPressed.connect(self.check_rightKey)
+        self.confirm_LineEdit.setVisible(False)
+        self.confirm_LineEdit.setPlaceholderText("Enter Key here: 30' remain")
+        self.Confirm_key = None
+
+        # threads
+        self.send_Email_Thread = None
+        self.wait = wait_Toclear_thread(self)
+        self.wait.isFinished.connect(self.clear_msg)
+        self.wait_Toconfirm = wait_Toconfirm(self)
+        self.wait_Toconfirm.Finished_1s.connect(self.Update_Time)
+        self.wait_Toconfirm.Finished_Time.connect(self.Time_Ended)
+
 
     def StudentID_update(self, val):
         self.StudentID = val
-        self.label_Error.clear()
         self.lineEdit_StudentID.setStyleSheet("")
         if self.StudentID:
             self.clearButton.setEnabled(True)
             if self.lineEdit_Email.text():
                 self.LoginButton.setEnabled(True)
+                self.signButton.setEnabled(True)
         else:
             if not self.lineEdit_Email.text():
                 self.clearButton.setEnabled(False)
             self.LoginButton.setEnabled(False)
+            self.signButton.setEnabled(False)
 
     def Email_update(self, email):
         self.Email = email.lower()
-        self.label_Error.clear()
         self.lineEdit_Email.setStyleSheet("")
         if self.Email:
             self.clearButton.setEnabled(True)
             if self.lineEdit_StudentID.text():
                 self.LoginButton.setEnabled(True)
+                self.signButton.setEnabled(True)
         else:
             if not self.lineEdit_StudentID.text():
                 self.clearButton.setEnabled(False)
             self.LoginButton.setEnabled(False)
+            self.signButton.setEnabled(False)
 
     def clear(self):
         self.lineEdit_StudentID.clear()
@@ -77,9 +103,128 @@ class LoginWindow(QDialog, Form):
             self.close()
             return True
         else:
-            self.label_Error.setText("Please try again")
             self.lineEdit_StudentID.setStyleSheet("background:#ff967c")
             self.lineEdit_Email.setStyleSheet("background:#ff967c")
+
+    def signUp(self):
+        # self.setEnabled(False)
+        self.signButton.setEnabled(False)
+        print("sign")
+        if self.check_valid_Email(self.Email):
+            self.send_Email_Thread = send_Email(self, receiver= self.Email)
+            self.send_Email_Thread.Finished.connect(self.check_verify)
+            self.send_Email_Thread.start()
+        else:
+            self.signButton.setEnabled(True)
+            self.user_Message("Invalid Email", "rgb(255, 0, 0)")
+
+
+
+    def check_valid_Email(self, address):
+        if address.find("@") != -1:
+            return True
+        return False
+        
+
+    def check_verify(self, key):
+        if key:
+            self.Confirm_key = key
+            self.signButton.setEnabled(True)
+            print("Finished: ", key)
+            self.confirm_LineEdit.setVisible(True)
+            self.groupBox.setVisible(False) # invisible MailWindow
+            self.wait_Toconfirm.start()
+            self.user_Message("message sent", "rgb(0, 170, 0)")
+            
+        else:
+            self.signButton.setEnabled(True)
+            self.user_Message("Connection Faild, or invalid Email addres", "rgb(255, 0, 0)", font=8)
+
+
+            
+
+    def Update_Time(self, val):
+        self.confirm_LineEdit.setPlaceholderText(f"Enter Key here: {val}' remain")
+
+    def Time_Ended(self, key):
+        if key:
+            self.confirm_LineEdit.setVisible(False)
+            self.groupBox.setVisible(True) # visible MailWindow
+            
+
+    def check_rightKey(self):
+        try:
+            if int(self.confirm_LineEdit.text()) == self.Confirm_key:
+                print("correct key")
+                self.confirm_LineEdit.setVisible(False)
+                self.groupBox.setVisible(True) # visible MailWindow
+            else:
+                self.user_Message("Invalid Key", "rgb(255, 0, 0)")
+        except:
+            self.user_Message("Just Number Please", "rgb(255, 0, 0)", font= 10)
+
+
+
+
+    def user_Message(self, msg, color, font = 12):
+        self.Label_Msg.setVisible(True)
+        self.Label_Msg.setText(msg)
+
+        stylesheet1 = ("""
+            QLabel{
+                color: """ + color + ";"
+        )
+        stylesheet2 = ("""
+                font: """ + str(font) + "pt;}"
+        )
+        self.Label_Msg.setStyleSheet(stylesheet1 + stylesheet2)
+        self.wait.start()
+
+
+    def clear_msg(self, check):
+        if check:
+            self.Label_Msg.setVisible(False)
+
+
+    def forget_Pass(self, val):
+        print("forget pass")
+
+
+
+class send_Email(QtCore.QThread):
+    Finished = QtCore.pyqtSignal(int)
+    def __init__(self, window, receiver):
+        self.receiver = receiver
+        QtCore.QThread.__init__(self, parent= window)
+    def run(self):
+        confirm_key = signup.Send_Email(self.receiver)
+        self.Finished.emit(confirm_key)
+
+
+class wait_Toclear_thread(QtCore.QThread):
+    isFinished = QtCore.pyqtSignal(bool)
+    def __init__(self, window):
+        QtCore.QThread.__init__(self, parent= window)
+    def run(self):
+        sleep(2.5)
+        self.isFinished.emit(True)
+
+
+class wait_Toconfirm(QtCore.QThread):
+    Finished_1s = QtCore.pyqtSignal(int)
+    Finished_Time = QtCore.pyqtSignal(bool)
+    def __init__(self, window):
+        QtCore.QThread.__init__(self, parent= window)
+    
+    def run(self):
+        for i in range(30):
+            sleep(1)
+            self.Finished_1s.emit(30 - (i + 1))
+        self.Finished_Time.emit(True)
+
+
+
+
 
 
 if __name__ == "__main__":
---
+++ signupPart/CustomLineEdit.py
@@ -0,0 +1,19 @@
+from PyQt5.QtWidgets import QLineEdit
+
+
+class LineEdit(QLineEdit):
+    def __init__(self, parent):
+        QLineEdit.__init__(self, parent= parent)
+        styleSheet_LineEdits = ("""
+            QLineEdit{ 
+                background-color: #b5e2ff;
+                border: 2px solid gray;
+                border-radius: 4px;
+                padding: 0 8px;
+                selection-background-color: darkgray;
+                font-size: 14px;
+            }
+        """)
+        self.setStyleSheet(styleSheet_LineEdits)
+        self.resize(250, 30)
+        self.move(parent.size().width() / 2 - 125, parent.size().height()/2 - 15)
